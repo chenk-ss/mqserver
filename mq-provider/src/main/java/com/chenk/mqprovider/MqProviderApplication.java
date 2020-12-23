7039adcb00711836f093e0cb8a1d014d6e9da9b3
chenk <chenk@jsyl.com.cn>
2020-12-23 21:40:37 +0800

    init project

---
+++ mq-provider/src/main/java/com/chenk/mqprovider/MqProviderApplication.java
@@ -0,0 +1,13 @@
+package com.chenk.mqprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqProviderApplication.class, args);
+    }
+
+}
